c9b72dcea92f2b8aa3baefbfe6745c01d207450e
Walter <walter13457@gmail.com>
2023-10-29 00:34:35 -0600

    Nosotros

--- src/views/Instalaciones.vue
+++ src/views/Instalaciones.vue
@@ -1,9 +1,52 @@
 <template>
-    <h1>Nuestras instalaciones</h1>
-</template>
+    <div class="instal12" >
+        <h1 class="hhhh1">Nuestras instalaciones</h1>
+            <div >
+            <img alt=" " src="../assets/3.jpeg" class="logo"> 
+            </div>
+            <div class="tttp box-container">
+            <p>Nuestas instalaciones han sido galardonadas por nuestras mejores atenciones por nuestros colaboradores en cual sera un gusto atenderle y de antemano muchas gracias por preferirnos. </p>
+            <p> Somos una solida organización financiera conscientes de nuestra responsabilidad para con nuestros clientes, nuestro personal, nuestra comunidad y nuestros accionistas. </p>
+            <p> Lo que empezó como un sueño de varios empresarios del sector industrial guatemalteco, con el tiempo se convirtió en el principal banco del sistema bancario guatemalteco. </p>
+            <p> Visitanos en la dirección 4ta calle 20-20 zona 1 de San Lucas, Sacatepequez.</p>
+        </div>
+    </div>
+            
+        
+    
+       
+       </template>
 
 <script>
 export default{
     name:'Instalaciones'
 }
 </script>
+
+
+<style>
+.instal12{
+ background-color: rgb(44, 42, 42);
+
+ width: 100%;
+ height: 34rem;
+}
+
+.logo{
+ width: 30%;
+ height: 15rem;
+ margin-left: 15rem;
+
+}
+.hhhh1{
+    margin-left: 15rem;
+}
+
+.tttp{
+ color: blanchedalmond;
+ width: 60%;
+ height: 50%;
+ margin-left: 30rem;
+}
+
+</style>
